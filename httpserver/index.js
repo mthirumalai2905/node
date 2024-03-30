02ed1a5fadb66631e4ecb5fe45242cf2ae812c56
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-03-30 16:22:53 +0530

    Updated api

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   //console.log(req.url);
@@ -8,8 +9,14 @@ const server = http.createServer((req, res) => {
     res.end("Hello from the about page");
   } else if (req.url == "/contact") {
     res.end("Hello from the contact page");
+  } else if (req.url == "/api/users") {
+    fs.readFile(`${__dirname}/userApi/dummy.json`, "utf-8", (err,data) => {
+      console.log(data);
+      res.end(data);
+    })
+
   } else {
-    res.writeHead(404, {"Content-type": "text/html" });
+    res.writeHead(404, { "Content-type": "text/html" });
     res.end("<h1>This page doesnt exist</h1>");
   }
 });
